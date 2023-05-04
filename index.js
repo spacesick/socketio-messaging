6a4cb4a739337142731950432412fdaf25117922
Spacesick <vincent.yovian01@ui.ac.id>
2023-05-04 10:59:49 +0700

    Initialize prisma client

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
@@ -5,6 +8,7 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
-httpServer.listen(process.env.PORT, () =>
-  console.log(`Listening on http://localhost:${process.env.PORT}/`)
-);
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Listening on http://localhost:${process.env.PORT}/`);
+  console.log("CTRL/CMD + C to stop the server");
+});
