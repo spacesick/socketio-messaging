761a4b98d63843aac9889d2538e67912201867d4
Spacesick <vincent.yovian01@ui.ac.id>
2023-05-05 10:21:38 +0700

    Add socket middleware and connection event listener

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
+io.use((socket, next) => {
+  socket.userId = socket.handshake.auth.userId;
+  socket.username = socket.handshake.auth.username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  socket.join(socket.username);
+});
+
 httpServer.listen(process.env.PORT, () => {
   console.log(`Listening on http://localhost:${process.env.PORT}/`);
   console.log("CTRL/CMD + C to stop the server");
